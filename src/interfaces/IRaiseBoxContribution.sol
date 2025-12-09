7abbbf16cd9068e0154436224b68ab5ebc188855
0xcoda <tech.codemojo@gmail.com>
2025-12-09 15:44:04 +0100

    clean up

--- src/interfaces/IRaiseBoxContribution.sol
+++ src/interfaces/IRaiseBoxContribution.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.30;
 
 /*
     Note: this is the interface for the RaiseBoxCreation Contribution contract
@@ -17,24 +17,26 @@ interface IRaiseBoxContribution {
     error RaiseBoxContribution_contribute_AboveMaxAllowed(uint256, string);
     error RaiseBoxContribution_getMaxContributionAllowedForProject_CannotBeZero();
     error RaiseBoxContribution_SelfContribution();
-    error RaiseBoxContribution_NotAContributor(bytes32 projectId);
+    error RaiseBoxContribution_NotAContributor(bytes32 raiseId);
 
     // contribution related events:
-    event Contributed(address indexed user, uint256 indexed amountContributed, bytes32 indexed projectId, uint256 amountRaised);
+    event Contributed(
+        address indexed user, uint256 indexed amountContributed, bytes32 indexed raiseId, uint256 amountRaised
+    );
 
-    function contribute(uint256 amount, bytes32 projectId) external payable;
+    function contribute(uint256 amount, bytes32 raiseId) external payable;
 
-    function getContributors(bytes32 projectId) external view returns (address[] memory);
+    function getContributors(bytes32 raiseId) external view returns (address[] memory);
 
-    function getMaxContributionAllowedForProject(bytes32 projectId) external returns (uint256);
+    function getMaxContributionAllowedForProject(bytes32 raiseId) external returns (uint256);
 
-    function getTotalContributionsToProject(bytes32 projectId) external returns (uint256 contributions);
+    function getTotalContributionsToProject(bytes32 raiseId) external returns (uint256 contributions);
 
-    function getContributionsToProject(address user, bytes32 projectId) external returns (uint256[] memory);
+    function getContributionsToProject(address user, bytes32 raiseId) external returns (uint256[] memory);
 
-    function getContributorsCount(bytes32 projectId) external returns (uint256 contributorCount);
+    function getContributorsCount(bytes32 raiseId) external returns (uint256 contributorCount);
 
-    function getHasContributed(bytes32 projectId, address user) external view returns (bool);
+    function getHasContributed(bytes32 raiseId, address user) external view returns (bool);
 
-    function getRaiseContributorsCount(bytes32 projectId) external returns (uint256);
+    function getRaiseContributorsCount(bytes32 raiseId) external returns (uint256);
 }
